255001fe198112e47b63714f0611c455d8312256
RaghvendraPal <raghvendrapal65@gmail.com>
2020-04-24 10:13:04 +0530

    'push'

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + 'front-end/dist/'));
+app.use(express.static('./dist/pool-carz'));
 app.listen(process.env.PORT || 8080);
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/dist/pool-carz/index.html'));
-// });
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname,   '/dist/pool-carz/index.html'));
 });
 
 console.log('Console listening');
